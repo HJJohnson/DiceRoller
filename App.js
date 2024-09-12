7d8fbb5e511fd033febf692dc26321071519942d
HJJohnson <heather.johnson@nwktc.edu>
2024-09-12 08:16:48 -0600

    first commin for kenli!

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+const diceImages = [
+  require('./assets/images/dice1.png'), 
+  require('./assets/images/dice2.png'), 
+  require('./assets/images/dice3.png'),
+  require('./assets/images/dice4.png'),
+  require('./assets/images/dice5.png'),
+  require('./assets/images/dice6.png')
+];
 
 export default function App() {
+  const [diceIndex, setDiceIndex] = useState(0);
+
+   const rollDice = () => {
+    const randomNumber = Math.floor(Math.random() * 6);
+    setDiceIndex(randomNumber);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={rollDice}>
+        <Image source={diceImages[diceIndex]} style={styles.diceImage} />
+      </TouchableOpacity>
+      <Text style={styles.instruction}>Tap the dice to roll!</Text>
     </View>
   );
 }
@@ -13,8 +32,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  diceImage: {
+    width: 150,
+    height: 150,
+  },
+  instruction: {
+    marginTop: 20,
+    fontSize: 18,
   },
 });
+
